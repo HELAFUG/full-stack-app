a4a137b5868d81c8a089a73d8bf21005921f9966
HELAFUG <loxand42@gmail.com>
2025-12-06 01:07:42 +0300

    made a base methods

--- backend/app/repository/todo.py
+++ backend/app/repository/todo.py
@@ -1,1 +1,22 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from core.models import Todo
+
+
+async def create_todo(session: AsyncSession, todo: Todo) -> Todo:
+    session.add(todo)
+    await session.commit()
+    await session.refresh(todo)
+    return todo
+
+
+async def get_todos(session: AsyncSession) -> list[Todo]:
+    stmt = select(Todo).order_by(Todo.id)
+    res = await session.execute(stmt)
+    return res.scalars().all()
+
+
+async def get_todo(session: AsyncSession, todo_id: int) -> Todo:
+    stmt = select(Todo).where(Todo.id == todo_id)
+    res = await session.execute(stmt)
+    return res.scalars().one()
