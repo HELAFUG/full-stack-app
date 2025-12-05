1088a18ff4ec35169d60c4e64cf82734d612372b
HELAFUG <loxand42@gmail.com>
2025-12-06 00:31:40 +0300

    made a database helper

---
+++ backend/app/core/models/db_helper.py
@@ -0,0 +1,31 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from core.config import settings
+
+
+class DBHelper:
+    def __init__(self, url: str, echo: bool) -> None:
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
+        )
+
+        self.session_factory = async_sessionmaker(
+            engine=self.engine,
+            expire_on_commit=False,
+            autoflush=False,
+            autocommit=False,
+        )
+
+    async def dispose(self) -> None:
+        await self.engine.dispose()
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+
+
+db_helper = DBHelper(
+    url=settings.db.url,
+    echo=settings.db.echo,
+)
