e85782b4c58f2ee6b9308f2cbc28d9b25c2aca66
HELAFUG <loxand42@gmail.com>
2025-12-06 00:13:59 +0300

    init react app

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,18 @@ import uvicorn
 from fastapi import FastAPI
 from api import api_router
 from core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
 
