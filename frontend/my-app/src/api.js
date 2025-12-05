e3aebcb8de344ac482c967a5240c17eba52c3ec4
HELAFUG <loxand42@gmail.com>
2025-12-06 01:23:20 +0300

    made api router methods

---
+++ frontend/my-app/src/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+
+const API_URL = 'http://127.0.0.1:8080/api/v1/todos'
+
+export const fetchTodos = () => axios.get(API_URL);
+export const createTodo = (todo) => axios.post(API_URL, todo);
+export const updateTodo = (todoId, updatedTodo) => axios.put(`${API_URL}/${todoId}`, updatedTodo);
+export const deleteTodo = (todoId) => axios.delete(`${API_URL}/${todoId}`);
